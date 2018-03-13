65d0e4269e28b418844deb13c14ba191a7fc0a51
amy <anmingyu@smartisan.com>
2018-03-13 15:39:01 +0800

    View release anim.
    Signed-off-by: amy <anmingyu@smartisan.com>

--- swipeitemlayout/src/main/java/com/amy/swipeitemlayout/SwipeItemLayout.java
+++ swipeitemlayout/src/main/java/com/amy/swipeitemlayout/SwipeItemLayout.java
@@ -2,6 +2,7 @@ package com.amy.swipeitemlayout;
 
 import android.content.Context;
 import android.support.v4.view.MotionEventCompat;
+import android.support.v4.view.ViewCompat;
 import android.support.v4.widget.ViewDragHelper;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
@@ -18,14 +19,27 @@ public class SwipeItemLayout extends FrameLayout {
         PullOut, LayDown
     }
 
+    public enum Status {
+        Opened, Closed, Moving
+    }
+
     private static boolean isBlocking = true;
     // 顶部视图
     private View mTopView;
     // 底部视图
     private View mBottomView;
 
+    // 滑动控件当前的状态（打开，关闭，正在移动），默认是关闭状态
+    private Status mCurrentStatus = Status.Closed;
+    // 滑动控件滑动前的状态
+    private Status mPreStatus = mCurrentStatus;
+
+    //拖动比率
+    private float mDragRatio;
+
     //layout之魂
     private int mTopLeft;
+
     // 顶部视图外边距
     private MarginLayoutParams mTopLp;
     // 底部视图外边距
@@ -64,6 +78,23 @@ public class SwipeItemLayout extends FrameLayout {
             }
 
             @Override
+            public void onViewDragStateChanged(int state) {
+                super.onViewDragStateChanged(state);
+            }
+
+            @Override
+            public int getViewVerticalDragRange(View child) {
+                return 0;
+            }
+
+            /*
+            @Override
+            public int getOrderedChildIndex(int index) {
+                return indexOfChild(mTopView);
+            }
+            */
+
+            @Override
             public int getViewHorizontalDragRange(View child) {
                 return mDragRange + mSpringDistance;
             }
@@ -94,16 +125,94 @@ public class SwipeItemLayout extends FrameLayout {
             public void onViewPositionChanged(View changedView, int left, int top, int dx, int dy) {
                 mTopLeft = left;
 
-                requestLayout();
+                int topViewHorizontalOffset = Math.abs(mTopLeft - (getPaddingLeft() + mTopLp.leftMargin));
+                if (topViewHorizontalOffset > mDragRange) {
+                    mDragRatio = 1.0f;
+                } else {
+                    mDragRatio = 1.0f * topViewHorizontalOffset / mDragRange;
+                }
+
+                dispatchSwipeEvent();
+
+                //Todo requestLayout();通过offsetLeftAndRight来移动view,requestLayout需要时间
+                if (mLayoutModel == LayoutModel.PullOut) {
+                    ViewCompat.offsetLeftAndRight(mBottomView, dx);
+                    ViewCompat.offsetTopAndBottom(mBottomView, dy);
+                }
             }
 
             @Override
             public void onViewReleased(View releasedChild, float xvel, float yvel) {
-                super.onViewReleased(releasedChild, xvel, yvel);
+                if (releasedChild != mTopView) {
+                    super.onViewReleased(releasedChild, xvel, yvel);
+                }
+                LogUtil.d("onReleased childView : " + (releasedChild == mTopView ? " topView " : " bottomView "));
+
+                final float dragRatioBottom = mReleaseRatio;
+                final float dragRatioTop = 1 - mReleaseRatio;
+
+                int topFinalLeft = getPaddingLeft() + mTopLp.leftMargin;
+                if (mSwipeDirection == SwipeDirection.Left) {
+                    //向左滑动为打开，向右滑动为关闭
+                    if (xvel < -VEL_THRESHOLD
+                            || (mPreStatus == Status.Closed && xvel < VEL_THRESHOLD && mDragRatio >= dragRatioBottom)
+                            || (mPreStatus == Status.Opened && xvel < VEL_THRESHOLD && mDragRatio >= dragRatioTop)) {
+                        // 向左的速度达到条件
+                        topFinalLeft -= mDragRange;
+                    }
+                } else {
+                    //向左滑动为关闭，向右滑动为打开
+                    if (xvel > VEL_THRESHOLD
+                            || (mPreStatus == Status.Closed && xvel > -VEL_THRESHOLD && mDragRatio >= dragRatioBottom)
+                            || (mPreStatus == Status.Opened && xvel > -VEL_THRESHOLD && mDragRatio >= dragRatioTop)) {
+                        topFinalLeft += mDragRange;
+                    }
+                }
+
+                mDragHelper.settleCapturedViewAt(topFinalLeft, getPaddingTop());
+
+                ViewCompat.postInvalidateOnAnimation(SwipeItemLayout.this);
             }
         };
 
         mDragHelper = ViewDragHelper.create(this, 1.0f, mDragHelperCallBack);
+    }
+
+    @Override
+    public void computeScroll() {
+        if (mDragHelper.continueSettling(false)) {
+            ViewCompat.postInvalidateOnAnimation(this);
+        }
+        super.computeScroll();
+    }
+
+    private void dispatchSwipeEvent() {
+        //Status preStatus = mCurrentStatus;
+        updateCurrentStatus();
+    }
+
+    private void updateCurrentStatus() {
+        if (mSwipeDirection == SwipeDirection.Left) {
+            // 向左滑动
+
+            if (mTopLeft == getPaddingLeft() + mTopLp.leftMargin - mDragRange) {
+                mCurrentStatus = Status.Opened;
+            } else if (mTopLeft == getPaddingLeft() + mTopLp.leftMargin) {
+                mCurrentStatus = Status.Closed;
+            } else {
+                mCurrentStatus = Status.Moving;
+            }
+        } else {
+            // 向右滑动
+
+            if (mTopLeft == getPaddingLeft() + mTopLp.leftMargin + mDragRange) {
+                mCurrentStatus = Status.Opened;
+            } else if (mTopLeft == getPaddingLeft() + mTopLp.leftMargin) {
+                mCurrentStatus = Status.Closed;
+            } else {
+                mCurrentStatus = Status.Moving;
+            }
+        }
     }
 
     @Override
@@ -116,7 +225,6 @@ public class SwipeItemLayout extends FrameLayout {
 
         // 避免底部视图被隐藏时还能获取焦点被点击
         //Todo : mBottomView.setVisibility(INVISIBLE);
-
 
         mTopView = getChildAt(1);
         mBottomView = getChildAt(0);
@@ -211,6 +319,30 @@ public class SwipeItemLayout extends FrameLayout {
      */
     public static void openBlockMode(boolean open) {
         isBlockMode = open;
+    }
+
+    /**
+     * 默认的fling速度阈值
+     */
+    public static final int DEFAULT_VEL_THRESHOLD = 400;
+    private int VEL_THRESHOLD = DEFAULT_VEL_THRESHOLD;
+
+    public void setVelThreshold(int velThreshold) {
+        if (velThreshold < 0) {
+            throw new IllegalArgumentException("vel cannot be < 0.");
+        }
+        VEL_THRESHOLD = velThreshold;
+    }
+
+    public static final float DEFAULT_RELEASE_RATIO = 0.4f;
+    public float mReleaseRatio = DEFAULT_RELEASE_RATIO;
+
+    public void setReleaseRatio(float ratio) {
+        if (ratio > 1.0f || ratio < 0.0f) {
+            throw new IllegalArgumentException("release ratio must between 0 to 1");
+        }
+
+        mReleaseRatio = ratio;
     }
 
     /**
